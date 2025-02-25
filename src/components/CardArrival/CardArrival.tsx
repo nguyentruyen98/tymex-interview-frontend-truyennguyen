b03b4dab8d56e06daa28ba9dcbe63648a1fff7ef
nguyentruyen98 <haohaothieugia@gmail.com>
2025-02-26 01:09:45 +0700

    feat: Add CardArrival component with 3D card effects and avatar support

---
+++ src/components/CardArrival/CardArrival.tsx
@@ -0,0 +1,30 @@
+import backGround from "@/assets/images/card_arrival_bg.png";
+import Text from "@/components/Text/Text";
+import { CardContainer, CardItem } from "@/components/ui/3d-card";
+
+import { CardArrivalProps } from "./CardArrival.type";
+
+const CardArrival = ({ name, avatar }: CardArrivalProps) => {
+  return (
+    <CardContainer className="inline-flex cursor-pointer flex-col items-center">
+      <CardItem translateZ="50" className="relative">
+        <img
+          className="relative z-10 h-[168px] w-[200px] object-cover object-top"
+          src={avatar}
+          alt="Avatar"
+        />
+        <img
+          src={backGround}
+          className="absolute bottom-[-1px] h-[120px] w-[200px] border border-[#FBC625] object-cover shadow-[-12px_12px_#101010]"
+        />
+      </CardItem>
+      <CardItem translateZ="20">
+        <Text className="mt-6" variant="title-lg" color="secondary">
+          {name}
+        </Text>
+      </CardItem>
+    </CardContainer>
+  );
+};
+
+export default CardArrival;
---
+++ src/components/CardArrival/CardArrival.type.ts
@@ -0,0 +1,1 @@
+export type CardArrivalProps = { name: string; avatar: string };
