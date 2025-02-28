18a70ecd1a0f198f0daff5904c5c9c4a003da43a
truyen-infotrack-vn <truyen.nguyen@infotrack.com.au>
2025-02-28 11:10:02 +0700

    feat: Add CardItemSkeleton component for displaying skeleton loading state

---
+++ src/components/CardItemSkeleton/CardItemSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { withFadeIn } from "@/hoc/withFadeIn";
+
+const CardItemSkeleton = () => {
+  return (
+    <div className="bg-shadow-violet/60 flex h-[377px] w-[267px] flex-col items-center rounded-[10px] p-4">
+      <Skeleton className="mt-4 h-[233px] w-[235px]" />
+      <Skeleton className="mt-6 h-6 w-full" />
+      <div className="mt-4 flex w-full items-center justify-start gap-3">
+        <Skeleton className="h-8 w-8 rounded-full" />
+        <Skeleton className="h-4 w-[120px]" />
+      </div>
+    </div>
+  );
+};
+
+const CardItemSkeletonWithAnimation = withFadeIn(CardItemSkeleton);
+
+export default CardItemSkeletonWithAnimation;
